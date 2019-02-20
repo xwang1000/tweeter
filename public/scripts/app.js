b62aea25e2847eaa97bf8963b68154b050ae3296
xw <23066125+xwang1000@users.noreply.github.com>
2019-02-20 15:22:21 -0800

    Add form submission using aJax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,18 @@
 $(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
-const createTweetElement = tweet => {
+  
+  const createTweetElement = tweet => {
   const $tweet = $('<article>').addClass('tweet')
   $tweet.html(`
     <header>
-            <img class="profile-pic" src=${tweet['user']['avatars']['small']} alt="sample profile picture">
-            <p class="username">${tweet['user']['name']}</p>
-            <p class="userid">${tweet['user']['handle']}</p>
+            <img class="profile-pic" src=${tweet.user.avatars.small} alt="sample profile picture">
+            <p class="username">${tweet.user.name}</p>
+            <p class="userid">${tweet.user.handle}</p>
           </header>
           <div class="tweet-body">
-           ${tweet['content']['text']}
+           ${tweet.content.text}
           </div>
           <footer>
-            <p>${new Date(tweet['created_at'])}</p>
+            <p>${new Date(tweet.created_at)}</p>
           </footer>
   `)
   return $tweet
@@ -67,9 +21,28 @@ const createTweetElement = tweet => {
 // Take an array of tweets
 const renderTweets = tweets => {
   tweets.forEach(tweetData => {
-    $('#tweets-container').append(createTweetElement(tweetData))
+    $('#tweets-container').prepend(createTweetElement(tweetData))
   });
 }
 
-renderTweets(data);
+// Form submission using ajax
+$('#tweet-composer').on('submit', function(event) {
+  event.preventDefault()
+  const input = $(this).serialize()
+  
+  // Needs refactor
+  $.post('/tweets', input, () => {
+    loadTweets()
+  })
+})
+
+const loadTweets = () => {
+  $.get('/tweets', (tweets) => {    
+    console.log(tweets)
+    renderTweets(tweets)
+  })
+}
+
+loadTweets()
+
 })
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -12,7 +12,7 @@ module.exports = function(DataHelpers) {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
-        res.json(tweets);
+        res.json(tweets)
       }
     });
   });
