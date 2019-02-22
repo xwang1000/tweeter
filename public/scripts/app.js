a2e33f36dd66bb2173bda722e98c8a51cee8be08
xw <23066125+xwang1000@users.noreply.github.com>
2019-02-21 22:39:43 -0800

    Add three icons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,138 +2,144 @@ $(() => {
   
   // Takes a tweet object and create its node element
   const createTweetElement = tweet => {
-    const $tweet = $('<article>').addClass('tweet')
-    $tweet.html(`
-      <header>
-        <img class="profile-pic" src=${tweet.user.avatars.small} alt="small avatar">
-        <p class="username">${tweet.user.name}</p>
-        <p class="userid">${tweet.user.handle}</p>
-      </header>
-      <div class="tweet-body">
-        ${escapeHtml(tweet.content.text)}
-      </div>
-      <footer>
-        <p>${getDisplayTime(new Date(tweet.created_at))}</p>
-      </footer>
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <img class="profile-pic" src=${tweet.user.avatars.small} alt="small avatar">
+          <p class="username">${tweet.user.name}</p>
+          <p class="userid">${tweet.user.handle}</p>
+        </header>
+        <div class="tweet-body">
+          ${escapeHtml(tweet.content.text)}
+        </div>
+        <footer>
+          <p class="created-at">${getDisplayTime(new Date(tweet.created_at))}</p>
+          <div class="icon-wrapper"">
+            <img class="icon" src="../images/flag.png" alt="flag icon">
+            <img class="icon" src="../images/retweet.png" alt="retweet icon">
+            <img class="icon" src="../images/like.png" alt="like icon">
+          </div>
+        </footer>
+      </article>
     `)
     return $tweet
   }
 
-// Render all tweet nodes
-const renderTweets = tweets => {
-  tweets.forEach(tweetData => {
-    $('#tweets-container').prepend(createTweetElement(tweetData))
-  });
-}
+  // Render all tweet nodes
+  const renderTweets = tweets => {
+    tweets.forEach(tweetData => {
+      $('#tweets-container').prepend(createTweetElement(tweetData))
+    });
+  }
 
-const loadTweets = () => {
-  $.get('/tweets', (tweets) => {   
-    renderTweets(tweets)
+  const loadTweets = () => {
+    $.get('/tweets', (tweets) => {   
+      renderTweets(tweets)
+    })
+  }
+
+  // Cleans up
+  const resetComposer = () => {
+    $('#tweet-composer-input').val('')
+    $('.counter').html(140)
+  }
+
+  // Sanitizes user input strings
+  const escapeHtml = str => {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  const showComposerError = ({header, body}) => {
+    console.log(header, body)
+    $('.error-message-header').text(header)
+    $('.error-message-body').text(body)
+    $('#error-message').slideToggle(400)
+  }
+
+  const pluralize = (word, n) => n === 1 ? word : word + 's'
+
+  const timeAgo = (n, unit) => `${n} ${pluralize(unit, n)} ago`
+
+  const getDisplayTime = dateObj => {
+    const today = new Date(Date.now())
+
+    const times = [
+      {
+        unit: 'year',
+        getValue: () => today.getFullYear() - dateObj.getFullYear()
+      },
+      {
+        unit: 'month',
+        getValue: () => today.getMonth() - dateObj.getMonth()
+      },
+      {
+        unit: 'day',
+        getValue: () => today.getDate() - dateObj.getDate()
+      },
+      {
+        unit: 'hour',
+        getValue: () => today.getHours() - dateObj.getHours()
+      },
+      {
+        unit: 'minute',
+        getValue: () => today.getMinutes() - dateObj.getMinutes()
+      },
+      {
+        unit: 'second',
+        getValue: () => today.getSeconds() - dateObj.getSeconds()
+      }
+    ]
+
+    for(let i = 0; i < times.length; i++) {
+      const {unit, getValue} = times[i]
+      const value = getValue()
+      if (value > 0) {
+        return timeAgo(value, unit)
+      }
+    }
+  }
+
+  // Event handlers
+
+  // Submission handler
+  $('#tweet-composer').on('submit', function(event) {
+    event.preventDefault()
+    const inputText = this.text.value
+
+    // Hide previous error message before submission
+    $('#error-message').hide('fast')
+
+    // Error checking
+    if (inputText.length === 0 || '') {
+      showComposerError({
+        header: 'Empty tweet:', 
+        body: 'please type something.'})
+      return
+    } else if (inputText.length > 140) {
+      showComposerError({
+        header: 'Tweet too long:', 
+        body: 'less than 140 characters please.'})
+      return
+    }
+
+    // Encode and submit
+    const inputSerial = $(this).serialize()
+    $.post('/tweets', inputSerial, () => {
+      loadTweets()
+      resetComposer()
+    })  
   })
-}
 
-// Cleans up
-const resetComposer = () => {
-  $('#tweet-composer-input').val('')
-  $('.counter').html(140)
-}
+  // Button listening
+  $('.compose-toggle-button').on('click', () => {
+    $('.new-tweet').slideToggle('fast')
+    $('#tweet-composer-input').focus()
+  })
 
-// Sanitizes user input strings
-const escapeHtml = str => {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-const showComposerError = ({header, body}) => {
-  console.log(header, body)
-  $('.error-message-header').text(header)
-  $('.error-message-body').text(body)
-  $('#error-message').slideToggle(400)
-}
-
-const pluralize = (word, n) => n === 1 ? word : word + 's'
-
-const timeAgo = (n, unit) => `${n} ${pluralize(unit, n)} ago`
-
-const getDisplayTime = dateObj => {
-  const today = new Date(Date.now())
-
-  const times = [
-    {
-      unit: 'year',
-      getValue: () => today.getFullYear() - dateObj.getFullYear()
-    },
-    {
-      unit: 'month',
-      getValue: () => today.getMonth() - dateObj.getMonth()
-    },
-    {
-      unit: 'day',
-      getValue: () => today.getDate() - dateObj.getDate()
-    },
-    {
-      unit: 'hour',
-      getValue: () => today.getHours() - dateObj.getHours()
-    },
-    {
-      unit: 'minute',
-      getValue: () => today.getMinutes() - dateObj.getMinutes()
-    },
-    {
-      unit: 'second',
-      getValue: () => today.getSeconds() - dateObj.getSeconds()
-    }
-  ]
-
-  for(let i = 0; i < times.length; i++) {
-    const {unit, getValue} = times[i]
-    const value = getValue()
-    if (value > 0) {
-      return timeAgo(value, unit)
-    }
-  }
-}
-
-// Event handlers
-
-// Submission handler
-$('#tweet-composer').on('submit', function(event) {
-  event.preventDefault()
-  const inputText = this.text.value
-
-  // Hide previous error message before submission
-  $('#error-message').hide('fast')
-
-  // Error checking
-  if (inputText.length === 0 || '') {
-    showComposerError({
-      header: 'Empty tweet:', 
-      body: 'please type something.'})
-    return
-  } else if (inputText.length > 140) {
-    showComposerError({
-      header: 'Tweet too long:', 
-      body: 'less than 140 characters please.'})
-    return
-  }
-
-  // Encode and submit
-  const inputSerial = $(this).serialize()
-  $.post('/tweets', inputSerial, () => {
-    loadTweets()
-    resetComposer()
-  })  
-})
-
-// Button listening
-$('.compose-toggle-button').on('click', () => {
-  $('.new-tweet').slideToggle('fast')
-  $('#tweet-composer-input').focus()
-})
-
-// Initial content and display of the page
-loadTweets()
-$('.new-tweet').toggle()
-$('#error-message').toggle()
+  // Initial content and display of the page
+  loadTweets()
+  $('.new-tweet').toggle()
+  $('#error-message').toggle()
 })
