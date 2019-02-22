0230bfd5e797ef188dc9a0a0e8dc0794d1f2d72c
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2019-02-22 00:41:27 +0000

    Use MongoDB for CRUD with Tweets

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -5,22 +5,19 @@ const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
-  return {
 
+  return {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection('tweets').insertOne(newTweet, callback(null, true))
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      const tweets = db.collection("tweets").find().toArray((err, tweetsArray) => {
+        callback(null, tweetsArray.sort(sortNewestFirst));
+      })
     }
 
   };
